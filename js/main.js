7dd424461698c997feeeefc5d87c3b475effea07
Christopher-Pierson <54875547+Christopher-Pierson@users.noreply.github.com>
2020-03-25 00:34:27 -0500

    Color Scales Finished

--- js/main.js
+++ js/main.js
@@ -19,16 +19,16 @@ window.onload = function(){
       .datum(400) // a single value is a datum
       .attr("width", function(d){ // rectangle width
             return d * 2; // 400 * 2 = 800
-        })
-        .attr("height", function(d){ // rectangle height
-            return d; // 400
-        })
-        .attr("class", "innerRect") // class name
-        .attr("x", 50) // position from left on the x (horizontal) axis
-        .attr("y", 50) // position from top on the y (vertical) axis
-        .style("fill", "#FFFFFF"); // fill color
+      })
+      .attr("height", function(d){ // rectangle height
+          return d; // 400
+      })
+      .attr("class", "innerRect") // class name
+      .attr("x", 50) // position from left on the x (horizontal) axis
+      .attr("y", 50) // position from top on the y (vertical) axis
+      .style("fill", "#FFFFFF"); // fill color
 
-  //console.log(innerRect
+  //console.log(innerRect)
 
   // create cityPop array
   var cityPop = [
@@ -50,26 +50,64 @@ window.onload = function(){
         }
     ];
 
-    // Example 2.6 line 3
-    var circles = container.selectAll(".circles") // create an empty selection
-        .data(cityPop) // here we feed in an array
-        .enter() // one of the great mysteries of the universe
-        .append("circle") // inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            // calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            // use the index to place each circle horizontally
-            return 90 + (i * 180);
-        })
-        .attr("cy", function(d){
-            // subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
-            return 450 - (d.population * 0.0005);
-        });
+    // above Example 2.8 line 20
+    var x = d3.scaleLinear() // create the scale
+        .range([90, 810]) // output min and max
+        .domain([0, 3]); // input min and max
+
+   // find the minimum value of the array
+   var minPop = d3.min(cityPop, function(d){
+       return d.population;
+   });
+
+   // find the maximum value of the array
+   var maxPop = d3.max(cityPop, function(d){
+       return d.population;
+   });
+
+   // scale for circles center y coordinate
+   var y = d3.scaleLinear()
+       .range([440, 95])
+       .domain([
+           minPop,
+           maxPop
+       ]);
+
+   // color scale generator
+   var color = d3.scaleLinear()
+       .range([
+           "#FDBE85",
+           "#D94701"
+       ])
+       .domain([
+           minPop,
+           maxPop
+       ]);
+
+  // Example 2.6 line 3
+  var circles = container.selectAll(".circles") // create an empty selection
+      .data(cityPop) // here we feed in an array
+      .enter() // one of the great mysteries of the universe
+      .append("circle") // inspect the HTML--holy crap, there's some circles there
+      .attr("class", "circles")
+      .attr("id", function(d){
+          return d.city;
+      })
+      .attr("r", function(d){
+          // calculate the radius based on population value as circle area
+          var area = d.population * 0.01;
+          return Math.sqrt(area/Math.PI);
+      })
+      .attr("cx", function(d, i){
+          // use the scale generator with the index to place each circle horizontally
+          return x(i);
+      })
+      .attr("cy", function(d){
+          return y(d.population);
+      })
+      .style("fill", function(d, i){ // add a fill based on the color scale generator
+          return color(d.population);
+      })
+      .style("stroke", "#000"); // black circle stroke
+  
 };
